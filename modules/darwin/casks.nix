d19bd45879d1833cbd2d8425647a000d8ad56ad0
Garrett Leber <lebergarrett@gmail.com>
2024-04-27 13:28:52 -0400

    working starter for darwin

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,14 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+
+  # Communication Tools
+  "discord"
+  "slack"
+  "zoom"
+
+  # Browsers
+  "google-chrome"
+]
---
+++ modules/shared/packages.nix
@@ -0,0 +1,41 @@
+{ pkgs }:
+
+with pkgs; [
+  python3
+  terraform
+  terraform-docs
+  kubectl
+
+  tmux-sessionizer
+
+  # archives
+  zip
+  unzip
+
+  # utils
+  ripgrep # recursively searches directories for a regex pattern
+  jq # A lightweight and flexible command-line JSON processor
+  fzf # A command-line fuzzy finder
+
+  # networking tools
+  mtr # A network diagnostic tool
+  dnsutils  # `dig` + `nslookup`
+  nmap # A utility for network discovery and security auditing
+
+  # misc
+  which
+  tree
+  gnupg
+  gnumake
+  fira-code-nerdfont
+
+  # nix related
+  #
+  # it provides the command `nom` works just like `nix`
+  # with more details log output
+  nix-output-monitor
+
+  btop  # replacement of htop/nmon
+  iotop # io monitoring
+  iftop # network monitoring
+]
