f9067fa0dc5c8a347882b589dd2b23a7fe7ce3b8
Garrett Leber <garrett.leber@openvpn.net>
2024-05-11 14:28:55 -0400

    Don't autoselect cmp suggestions

--- modules/shared/nixvim/completion/cmp.nix
+++ modules/shared/nixvim/completion/cmp.nix
@@ -41,7 +41,7 @@
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<CR>" = "cmp.mapping.confirm({ select = false })";
           "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
         };
       };
